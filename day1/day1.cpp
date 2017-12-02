14c64137b9ae6bee7f231644d67233f97b141a3e
Szymon <szymonosis@o2.pl>
2017-12-02 01:18:50 +0100

    Updated day1

--- day1/day1.cpp
+++ day1/day1.cpp
@@ -12,7 +12,7 @@ int main()
 	int sum1 = 0;
 	int sum2 = 0;
 	int ind1, ind2;
-	for (int i = 0; i < in.size() - 1; i++)
+	for (int i = 0; i < in.size(); i++)
 	{
 		ind1 = (i + 1) % in.size();
 		ind2 = (i + (in.size() / 2)) % in.size();
@@ -21,8 +21,6 @@ int main()
 		if (in[i] == in[ind2])
 			sum2 += (int)in[i] - 48;
 	}
-	if (in[in.size()-1] == in[0])
-		sum1 += (int)in[0] - 48;
 	cout << in << endl << "Part I: " << sum1 << endl << "Part II: " << sum2 << endl;
 	return 0;
 }
